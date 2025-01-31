493f7293c590d214616ab0612dc68ef1dbe89a2d
ACrazySteve <80903200+ACS-resources@users.noreply.github.com>
2025-01-31 22:58:47 +0800

    Update LangEnglish.js

--- src/text/lang/LangEnglish.js
+++ src/text/lang/LangEnglish.js
@@ -11,49 +11,49 @@ define(function () {
 			var alt_cases = ["honest", "hour", "hono"];
 			for (let i in alt_cases) {
 				if (l_word.indexOf(alt_cases[i]) == 0)
-					return "an";
+					return "";
 			}
 			
 			// Single letter word which should be preceeded by 'an'
 			if (l_word.length == 1) {
 				if ("aedhilmnorsx".indexOf(l_word) >= 0)
-					return "an";
+					return "";
 				else
-					return "a";
+					return "";
 			}
 			
 			// Capital words which should likely be preceeded by 'an'
 			if (word.match(/(?!FJO|[HLMNS]Y.|RY[EO]|SQU|(F[LR]?|[HL]|MN?|N|RH?|S[CHKLMNPTVW]?|X(YL)?)[AEIOU])[FHLMNRSX][A-Z]/)) {
-				return "an";
+				return "";
 			}
 			
 			// Special cases where a word that begins with a vowel should be preceeded by 'a'
 			regexes = [/^e[uw]/, /^onc?e\b/, /^uni([^nmd]|mo)/, /^u[bcfhjkqrst][aeiou]/]
 			for (let i in regexes) {
 				if (l_word.match(regexes[i]))
-					return "a"
+					return ""
 			}
 			
 			// Special capital words (UK, UN)
 			if (word.match(/^U[NK][AIEO]/)) {
-				return "a";
+				return "";
 			}
 			else if (word == word.toUpperCase()) {
 				if ("aedhilmnorsx".indexOf(l_word[0]) >= 0)
-					return "an";
+					return "";
 				else
-					return "a";
+					return "";
 			}
 			
 			// Basic method of words that begin with a vowel being preceeded by 'an'
 			if ("aeiou".indexOf(l_word[0]) >= 0)
-				return "an";
+				return "";
 			
 			// Instances where y follwed by specific letters is preceeded by 'an'
 			if (l_word.match(/^y(b[lor]|cl[ea]|fere|gg|p[ios]|rou|tt)/))
-				return "an";
+				return "";
 			
-			return "a";
+			return "";
 		},
 
 	};
